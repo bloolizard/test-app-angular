1295297b5b056565018900822e201ead88315eaf
Edwin Villanueva <edwincv0@gmail.com>
2014-11-14 07:44:28 -0800

    added forms for question, added new view controller to handle displaying tests

--- public/js/angular/controllers.js
+++ public/js/angular/controllers.js
@@ -4,6 +4,10 @@ angular.module('testMakerApp.controllers',[])
 })
 .controller('NewTestViewController', function($scope, communicateScope){
         communicateScope.title = 'NEW TEST';
+        $scope.addQuestion = function(event){
+            event.preventDefault();
+            console.log(event);
+        }
 })
 .controller('MyTestsViewController', function($scope, communicateScope){
         communicateScope.title = 'MY TESTS';
@@ -12,5 +16,10 @@ angular.module('testMakerApp.controllers',[])
 .controller('HeaderViewController', function($scope, communicateScope){
     communicateScope.$watch('title', function(){
         $scope.title = communicateScope.title;
+    });
+})
+.controller('RenderTestFormViewController', function($scope, communicateScope){
+    communicateScope.$watch('render_test', function(){
+        $scope.render_test = communicateScope.render_test;
     });
 });
